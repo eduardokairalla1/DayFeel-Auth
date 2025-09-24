4ce1362b29b3fbfde8343c9d0e5a091afa07d73f
Eduardo Kairalla <eduardokairalla123@gmail.com>
2025-09-24 20:49:38 -0300

    feat: Implement user retrieval and last login update methods in UsersRepository

--- dayfeel_auth/db/sqlalchemy/repository/users.py
+++ dayfeel_auth/db/sqlalchemy/repository/users.py
@@ -3,12 +3,18 @@ Users table respository.
 """
 
 # --- IMPORTS ---
+from datetime import datetime
+from datetime import timezone
 from dayfeel_auth.db.sqlalchemy.models.users import Users
 from dayfeel_auth.db.sqlalchemy.setup.db_connection_handler import DbConnectionHandler
 from dayfeel_auth.err.already_exists_error import AlreadyExistsError
 from dayfeel_auth.err.database_unavailable_error import DatabaseUnavailableError
 from sqlalchemy import Engine
 from sqlalchemy.exc import IntegrityError
+
+
+# --- TYPES ---
+from typing import Optional
 
 
 # --- CODE ---
@@ -55,6 +61,77 @@ class UsersRepository:
                 raise AlreadyExistsError({'entity': 'user',
                                           'local': 'database',
                                           'detail': e}) from e
+
+            # If database is unavailable: raise error
+            except Exception as e:
+                raise DatabaseUnavailableError(e) from e
+
+
+    def get_by_email(self, email: str) -> Optional[Users]:
+        """
+        Retrieves a user by email.
+
+        :param email: email associete with user.
+
+        :returns: User or None if not found.
+        """
+        # Open database connection
+        with DbConnectionHandler(self.__engine) as db:
+            try:
+                # Retrieves user by email from database
+                query = db.session.query(Users).filter(Users.email == email).one_or_none()
+
+                # Returns User ou None
+                return query
+
+            # If database is unavailable: raise error
+            except Exception as e:
+                raise DatabaseUnavailableError(e) from e
+
+
+    def get_by_id(self, user_id: int) -> Optional[Users]:
+        """
+        Retrieves a user by id.
+
+        :param user_id: Id associete by user.
+
+        :returns: User or None if not found.
+        """
+        # Open database connection
+        with DbConnectionHandler(self.__engine) as db:
+            try:
+                # Retrieves user by id from database
+                query = db.session.query(Users).filter(Users.id == user_id).one_or_none()
+
+                # Returns User ou None
+                return query
+
+            # If database is unavailable: raise error
+            except Exception as e:
+                raise DatabaseUnavailableError(e) from e
+
+
+    def update_last_login(self, user_id: int) -> None:
+        """
+        Update the last_login field of a specific user.
+
+        :param user_id: User's unique identificator.
+
+        :returns: None.
+        """
+        # Open database connection
+        with DbConnectionHandler(self.__engine) as db:
+            try:
+                # Update last_login field of the user
+                db.session.query(Users).filter(
+                    Users.id == user_id
+                ).update(
+                    {Users.last_login: datetime.now(timezone.utc)},
+                    synchronize_session=False
+                )
+
+                # Commit changes
+                db.session.commit()
 
             # If database is unavailable: raise error
             except Exception as e:
